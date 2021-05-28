77aeaa021c19b067c06d02959797eea098ed8619
Damrongdech <84860958+Damrongdech@users.noreply.github.com>
2021-05-28 18:45:22 +0700

    Initial commit

---
+++ src/pages/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container" style="text-align: center">
+    <h1>Portfolio</h1>
+    <p>---------------------------------------</p>
+    <div>
+      <router-link :to="{ name: 'profile' }">
+        <img src="@/assets/b1.png" />
+      </router-link>
+    </div>
+    <div>
+      <router-link :to="{ name: 'contact' }">
+        <img src="@/assets/b2.png" />
+      </router-link>
+    </div>
+    <p>---------------------------------------</p>
+  </div>
+</template>
